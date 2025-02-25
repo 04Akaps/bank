1cbf891f9f13f7599ec8e196e5272644ecf11a1e
jukas <jukas@wemade.com>
2025-02-25 10:58:51 +0900

    Feat: add jwt provider

--- src/main/kotlin/common/exception/ErrorCode.kt
+++ src/main/kotlin/common/exception/ErrorCode.kt
@@ -4,5 +4,11 @@ enum class ErrorCode(
     override val code : Int,
     override var message : String
 ) : CodeInterface {
-    FailedToLoggerInvoke(-100, "Failed to logger invoke")
+    FailedToLoggerInvoke(-100, "Failed to logger invoke"),
+
+
+
+    TOKEN_IS_INVALID(-200, "Token is invalid"),
+    TOKEN_IS_EXPIRED(-201, "Token is expired"),
+    ACCESS_TOKEN_IS_NOT_EXPIRED(-202, "Access token is not expired"),
 }
